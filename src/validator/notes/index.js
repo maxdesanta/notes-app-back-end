d942a2cbfbb171879dddad45ce16321743377a60
max.desanta <maxdesanta1@gmail.com>
2025-09-17 12:54:32 +0700

    data validation implemented

---
+++ src/validator/notes/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require("../../exceptions/InvariantError");
+const { NotePayLoadSchema } = require("./schema")
+
+const NotesValidator = {
+    validateNotePayload: (payload) => {
+        const validationResult = NotePayLoadSchema.validate(payload);
+
+        if (validationResult.error) {
+            throw InvariantError(validationResult.error.message);
+        }
+    }
+}
+
+module.exports = NotesValidator;
