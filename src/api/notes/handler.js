642e1a2e61390b04900a96b24b81fbfc878aa7bd
max.desanta <maxdesanta1@gmail.com>
2025-09-22 10:42:13 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -14,43 +14,45 @@ class NotesHandler {
     
     async postNoteHandler(request, h) {
         try {
-        this._validator.validateNotePayload(request.payload);
-        const { title = 'untitled', body, tags } = request.payload;
+            this._validator.validateNotePayload(request.payload);
+            const { title = 'untitled', body, tags } = request.payload;
+            const { id: credentialId } = request.auth.credentials;
     
-        const noteId = await this._service.addNote({ title, body, tags });
-    
-        const response = h.response({
-            status: 'success',
-            message: 'Catatan berhasil ditambahkan',
-            data: {
-            noteId,
-            },
-        });
-        response.code(201);
-        return response;
-        } catch (error) {
-        if (error instanceof ClientError) {
+            const noteId = await this._service.addNote({ title, body, tags, owner: credentialId });
+        
             const response = h.response({
-            status: 'fail',
-            message: error.message,
+                status: 'success',
+                message: 'Catatan berhasil ditambahkan',
+                data: {
+                noteId,
+                },
             });
-            response.code(error.statusCode);
+            response.code(201);
             return response;
-        }
-    
-        // Server ERROR!
-        const response = h.response({
-            status: 'error',
-            message: 'Maaf, terjadi kegagalan pada server kami.',
-        });
-        response.code(500);
-        console.error(error);
-        return response;
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
         }
     }
     
     async getNotesHandler() {
-        const notes = await this._service.getNotes();
+        const { id: credentialId } = request.auth.credentials;
+        const notes = await this._service.getNotes(credentialId);
         return {
         status: 'success',
         data: {
@@ -61,14 +63,17 @@ class NotesHandler {
     
     async getNoteByIdHandler(request, h) {
         try {
-        const { id } = request.params;
-        const note = await this._service.getNotesById(id);
-        return {
-            status: 'success',
-            data: {
-            note,
-            },
-        };
+            const { id } = request.params;
+            const { id: credentialId } = request.auth.credentials;
+        
+            await this._service.verifyNoteAccess(id, credentialId);
+            const note = await this._service.getNotesById(id);
+            return {
+                status: 'success',
+                data: {
+                note,
+                },
+            };
         } catch (error) {
         if (error instanceof ClientError) {
             const response = h.response({
@@ -92,63 +97,68 @@ class NotesHandler {
     
     async putNoteByIdHandler(request, h) {
         try {
-        this._validator.validateNotePayload(request.payload);
-        const { id } = request.params;
-    
-        await this._service.editNoteById(id, request.payload);
-    
-        return {
-            status: 'success',
-            message: 'Catatan berhasil diperbarui',
-        };
+            this._validator.validateNotePayload(request.payload);
+            const { id } = request.params;
+            const { id: credentialId} = request.auth.credentials;
+            
+            await this._service.verifyNoteOwner(id, credentialId);    
+            await this._service.editNoteById(id, request.payload);
+        
+            return {
+                status: 'success',
+                message: 'Catatan berhasil diperbarui',
+            };
         } catch (error) {
-        if (error instanceof ClientError) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
             const response = h.response({
-            status: 'fail',
-            message: error.message,
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
             });
-            response.code(error.statusCode);
+            response.code(500);
+            console.error(error);
             return response;
-        }
-    
-        // Server ERROR!
-        const response = h.response({
-            status: 'error',
-            message: 'Maaf, terjadi kegagalan pada server kami.',
-        });
-        response.code(500);
-        console.error(error);
-        return response;
         }
     }
     
     async deleteNoteByIdHandler(request, h) {
         try {
-        const { id } = request.params;
-        await this._service.deleteNoteById(id);
+            const { id } = request.params;
+            const { id: credentialId } = request.auth.credentials;
+
+            await this._service.verifyNoteOwner(id, credentialId);
+            await this._service.deleteNoteById(id);
     
-        return {
-            status: 'success',
-            message: 'Catatan berhasil dihapus',
-        };
+            return {
+                status: 'success',
+                message: 'Catatan berhasil dihapus',
+            };
         } catch (error) {
-        if (error instanceof ClientError) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
             const response = h.response({
-            status: 'fail',
-            message: error.message,
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
             });
-            response.code(error.statusCode);
+            response.code(500);
+            console.error(error);
             return response;
-        }
-    
-        // Server ERROR!
-        const response = h.response({
-            status: 'error',
-            message: 'Maaf, terjadi kegagalan pada server kami.',
-        });
-        response.code(500);
-        console.error(error);
-        return response;
         }
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,35 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NoteService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UserServices = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+// authentication
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
+// collaborations
+const collaborations = require('./api/collaborations');
+const CollaborationsService = require('./services/postgres/CollaborationsService');
+const CollaborationsValidator = require('./validator/collaborations');
+
 const init = async () => {
-    const notesService = new NotesService();
+    const collaborationsService = new CollaborationsService();
+    const notesService = new NotesService(collaborationsService);
+    const userService = new UserServices();
+    const authenticationsService = new AuthenticationsService();
+
     const server = Hapi.server({
         port: process.env.PORT,
         host: process.env.HOST !== 'production' ? 'localhost' : '0.0.0.0',
@@ -16,14 +39,63 @@ const init = async () => {
         },
         },
     });
-    
-    await server.register({
-        plugin: notes,
-        options: {
-            service: notesService,
-            validator: NotesValidator,
+
+    await server.register([
+        {
+            plugin: Jwt, 
+        }
+    ]);
+
+    // mendefinisikan strategy autentikasi jwt
+    server.auth.strategy('notesapp_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
         },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
+
+    await server.register([
+        {
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            },
+        },
+        {
+            plugin: users,
+            options: {
+                service: userService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                userService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
+        {
+            plugin: collaborations,
+            options: {
+                collaborationsService,
+                notesService,
+                validator: CollaborationsValidator,
+            },
+        }
+    ]);
     
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
